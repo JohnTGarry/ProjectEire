dd5e9d3b24ee2a5cea1ab9798bdacfca53a7f3f6
RivalGarry <johntgarry@gmail.com>
2022-12-03 21:39:51 +0000

    horizontal dash

--- Assets/Player/Scripts/PlayerMovement.cs
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -11,44 +11,57 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
 
+    [SerializeField] private TrailRenderer tr;
+    [SerializeField] private float dashingPower;
+    [SerializeField] private float dashingTime;
+    [SerializeField] private float dashingCooldown;
+
     private bool isFacingRight = true;
     private float horizontalInput;
     private bool isJumpPressed = false;
     private bool isJumpReleased = true;
 
-    void Update()
-    {
+    private bool canDash = true;
+    private bool isDashing;
+
+    void Update() {
+        if (isDashing) {
+            return;
+        }
+
         horizontalInput = Input.GetAxisRaw("Horizontal");
         isJumpPressed = Input.GetButtonDown("Jump");
         isJumpReleased = Input.GetButtonUp("Jump");
 
-        if (isJumpPressed && isGrounded())
-        {
+        if (isJumpPressed && isGrounded()) {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
         }
 
-        if (isJumpReleased && rb.velocity.y > 0f)
-        {
+        if (isJumpReleased && rb.velocity.y > 0f) {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash) {
+            StartCoroutine(Dash());
         }
 
         Flip();
     }
 
-    private void FixedUpdate()
-    {
+    private void FixedUpdate() {
+        if (isDashing) {
+            return;
+        }
+        
         MovePlayer();    
     }
 
-    private void MovePlayer()
-    {
+    private void MovePlayer() {
         rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
     }
 
-    private void Flip()
-    {
-        if (isFacingRight && horizontalInput < 0f || !isFacingRight && horizontalInput > 0f)
-        {
+    private void Flip() {
+        if (isFacingRight && horizontalInput < 0f || !isFacingRight && horizontalInput > 0f) {
             isFacingRight = !isFacingRight;
             Vector3 localScale = transform.localScale;
             localScale.x *= -1f;
@@ -56,8 +69,22 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private bool isGrounded()
-    {
+    private bool isGrounded() {
         return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private IEnumerator Dash() {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        tr.emitting = false;
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
     }
 }
